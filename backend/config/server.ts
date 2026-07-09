c77f04093bf3647c59f082406d872f2193a9e114
kanaan.ngutu <kanaan.ngutu@gmail.com>
2026-07-09 18:12:05 +1200

    config: enable proxy for secure cookies handshakes

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -4,6 +4,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server =>
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('URL'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
